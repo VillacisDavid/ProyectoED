64f6642db4d8521cef8cf9f2d814f905b97ef863
katherine ambrocio <katherine.ambrocio@unmsm.edu.pe>
2021-12-23 23:48:59 -0500

    Carpeta de detalles adicionales

---
+++ src/main/java/detalles/cargosdesignados.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package detalles;
+
+/**
+ *
+ * @author User
+ */
+public class cargosdesignados {
+    //Nivel, NombreCargo, idCargoJefe, Sueldo
+    public static Object[][] infoEmpleado
+            = {{1, "Gerente", -1, 60000.0},
+            {2, "CEO", 0, 15000.0},
+            {3, "Administrador General", 1, 16000.0},
+            {4, "Jefe de operaciones", 2, 16000.0},
+            {4, "Jefe financiero", 2, 10000.0},
+            {4, "Jefe de marketing", 2, 16000.0},
+            {4, "Asistente de marketing general", 2, 9000.0},
+            {4, "Jefe de recursos humanos", 2, 8000.0},
+            {4, "Asistenta social", 2, 5000.0},
+            {5, "Operation Staff", 3, 4000.0},
+            {5, "Supervisor de local", 4, 3000.0},
+            {5, "Jefe de rutas", 5, 3500.0},
+            {5, "Reparador", 6, 3000.0},
+            {5, "Ingenieros IT", 7, 2500.0},
+            {5, "Conductor", 8, 1600.0}
+            };
+    
+}
